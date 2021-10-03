0614736a0b4851a3a0beaf3f9dec2d0889e3c918
TMichael <js5859168@gmail.com>
2021-10-03 13:13:14 +0300

    Start working with Kotlin

--- app/src/main/java/com/inflames1986/weatherkotlinapp/MainActivity.kt
+++ app/src/main/java/com/inflames1986/weatherkotlinapp/MainActivity.kt
@@ -2,12 +2,14 @@ package com.inflames1986.weatherkotlinapp
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var button: Button
+    lateinit var edittext: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +17,11 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Weather App Started", Toast.LENGTH_SHORT).show()
 
         button = findViewById(R.id.first_action_btn)
-
         button.setOnClickListener {
             Toast.makeText(this, "Hello, I'm Working", Toast.LENGTH_SHORT).show()
         }
+
+        edittext = findViewById(R.id.edittext)
+        edittext.setText("Привет")
     }
 }
---
+++ app/src/main/java/com/inflames1986/weatherkotlinapp/Person.kt
@@ -0,0 +1,15 @@
+package com.inflames1986.weatherkotlinapp
+
+class Person (val firstName: String, val lastName: String, val Age: Int) {
+
+    private val a = 10
+    private var name = "Vladimir"
+    private val list: List<String> = ArrayList()
+
+    fun setInitValues() {
+
+        name = "Alexander"
+        println(name)
+
+    }
+}
