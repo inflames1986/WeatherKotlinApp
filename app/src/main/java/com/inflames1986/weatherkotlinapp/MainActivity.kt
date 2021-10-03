b26898d63e2a11185ff4251bcdbe796feea4a637
TMichael <js5859168@gmail.com>
2021-10-03 12:11:22 +0300

    Tasks 1-4 completed

--- app/src/main/java/com/inflames1986/weatherkotlinapp/MainActivity.kt
+++ app/src/main/java/com/inflames1986/weatherkotlinapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.inflames1986.weatherkotlinapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Weather App Started", Toast.LENGTH_SHORT).show()
+
+        button = findViewById(R.id.first_action_btn)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Hello, I'm Working", Toast.LENGTH_SHORT).show()
+        }
     }
 }
